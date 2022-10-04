5706c13793c32de5af183e25d4176c23046b49df
sfertman <sfertman@zoho.com>
2022-10-04 17:35:07 -0400

    error middleware must accept four arguments (#454)

--- src/inventory/src/server.js
+++ src/inventory/src/server.js
@@ -65,7 +65,7 @@ function app(invPath=__dirname, config={}) {
             }, next);
     });
 
-    expressapp.use((err, req, res) => {
+    expressapp.use((err, req, res, next) => {
         if (err.constructor.name == "PayloadTooLargeError") {
             return res.status(413).send(err.message);
         }
